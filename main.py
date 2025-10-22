9c2f652088852c1367d1ff2d851116d8df232bad
heshber <heshber@gmail.com>
2025-10-22 20:38:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#from app.app import main
+
+#if __name__ == "__main__":
+#   main()
+import os
+
+def main():
+    os.system("streamlit run app/app.py")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='mymodule', packages=find_packages())
